9d0cf9608f121acdb194e86ec4c7c1227d9319b2
manuelechavarria <maechavarriaor@gmail.com>
2025-10-16 20:35:58 -0500

    CFA

--- src/config/global.js
+++ src/config/global.js
@@ -109,7 +109,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/212500_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
